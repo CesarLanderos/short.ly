aed5cd2e064df9378c365ce2761281056439ac9e
Cesar Landeros Delgado <chichozell@gmail.com>
2017-04-05 02:30:55 -0500

    created a view to list all the shortened urls in the system

---
+++ resources/views/partials/header.php
@@ -0,0 +1,24 @@
+<div class="container">
+    <nav class="nav">
+        <div class="nav-left">
+            <a class="nav-item title is-3" href="/">
+                short.ly
+            </a>
+        </div>
+
+        <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+            <a class="nav-item" href="/">
+                Home
+            </a>
+            <a class="nav-item" href="/all_urls">
+                Shortened urls
+            </a>
+        </div>
+    </nav>
+</div>
--- resources/views/urlList.php
+++ resources/views/urlList.php
@@ -2,9 +2,40 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Saved urls</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Saved urls | Short.ly!</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.min.css">
     </head>
     <body>
-
+        <?php require_once('partials/header.php') ?>
+        <main class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Original</th>
+                        <th>Shortened</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($urls as $url) : ?>
+                        <tr>
+                            <td><?= $url->url ?></td>
+                            <td>
+                                <a href="short.ly/<?= $url->code ?>">
+                                    short.ly/<?= $url->code ?>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Original</th>
+                        <th>Shortened</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </main>
     </body>
 </html>
